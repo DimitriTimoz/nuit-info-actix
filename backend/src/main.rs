1dd5340aa563cc0e853d240fad6f96e09467d7c8
DimitriTimoz <dimitri.timoz@orange.fr>
2023-12-05 16:28:44 +0100

    remove dangerous duplication

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -19,17 +19,6 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    DB.client
-        .execute(
-            "CREATE TABLE IF NOT EXISTS users (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR NOT NULL,
-                email VARCHAR NOT NULL
-            )",
-            &[],
-        )
-        .await
-        .unwrap();
     HttpServer::new(|| {
         App::new()
             .service(hello)
