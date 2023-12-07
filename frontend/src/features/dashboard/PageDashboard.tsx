53d3878cc36f8009c6b67959f116e616433554c8
Josselin Dulongcourty <contact@josselinonduty.fr>
2023-12-07 22:47:07 +0100

    feat: Add card playing page

--- frontend/src/features/dashboard/PageDashboard.tsx
+++ frontend/src/features/dashboard/PageDashboard.tsx
@@ -7,8 +7,20 @@ import {
   AlertTitle,
   Box,
   Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Center,
+  Grid,
+  HStack,
   Heading,
+  Image,
+  Img,
+  Progress,
+  StackDivider,
   Text,
+  VStack,
   Wrap,
 } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
@@ -23,13 +35,94 @@ export default function PageDashboard() {
 
   console.log(data);
   return (
-    <Page>
-      <PageContent>
-        <Heading size="md" mb="4">
-          {t('dashboard:title')}
-        </Heading>
-        {isLoading && <Loader />}
-        <Button onClick={() => refetch()}>Refetch</Button>
+    <Page containerSize="full">
+      <PageContent
+        mx={{
+          base: '4',
+          sm: '6',
+          md: '8',
+          lg: '10',
+        }}
+      >
+        <Grid
+          templateColumns={{ base: '1fr', md: '1fr 4fr 1fr' }}
+          templateRows={{ base: '1fr 1fr 1fr', md: '1fr' }}
+          gap="4"
+          placeItems="center"
+          justifyContent="center"
+        >
+          <Box
+            width="full"
+            height="full"
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            gap="4"
+          >
+            <Progress value={50} size="md" width="full" />
+            <Progress value={50} size="md" width="full" />
+            <Progress value={50} size="md" width="full" />
+          </Box>
+          <Box
+            display="flex"
+            width="full"
+            height="full"
+            flexDirection="column"
+            justifyItems="center"
+            alignItems="center"
+            justifyContent="center"
+            gap="4"
+          >
+            <Card
+              width={{ base: '90%', sm: '80%', md: '65%', lg: '50%' }}
+              height="fit-content"
+            >
+              <CardBody>
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  justifyItems="center"
+                >
+                  <Text textAlign="center">
+                    Culpa commodo velit proident do proident cupidatat in sint.
+                    Incididunt nostrud ipsum ut ullamco. Enim in adipisicing
+                    nostrud ea ex fugiat sit labore tempor voluptate amet
+                    proident.
+                  </Text>
+                  <Image
+                    aspectRatio={1 / 1}
+                    src="https://via.placeholder.com/150"
+                    m="4"
+                    rounded="md"
+                  />
+                </Box>
+              </CardBody>
+              <CardFooter display="flex" justify="center" gap="6">
+                <Button colorScheme="teal" size="sm">
+                  Accepter
+                </Button>
+                <Button colorScheme="teal" size="sm">
+                  Refuser
+                </Button>
+              </CardFooter>
+            </Card>
+          </Box>
+          <VStack display="flex" flexDirection="column" alignItems="center">
+            <Box>
+              <Heading size="md">Affinité faction 1</Heading>
+              <Text>50%</Text>
+            </Box>
+            <Box>
+              <Heading size="md">Affinité faction 2</Heading>
+              <Text>50%</Text>
+            </Box>
+            <Box>
+              <Heading size="md">Affinité faction 3</Heading>
+              <Text>50%</Text>
+            </Box>
+          </VStack>
+        </Grid>
       </PageContent>
     </Page>
   );
