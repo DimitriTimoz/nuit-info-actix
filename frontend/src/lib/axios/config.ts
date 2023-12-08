2601cbbc00ddc826b9b98e9bf1a36e4701652f9f
Dylan Flandrin <dylan.flandrin@bearstudio.fr>
2023-12-08 03:51:51 +0100

    fix: add token in axios config

--- frontend/src/lib/axios/config.ts
+++ frontend/src/lib/axios/config.ts
@@ -3,6 +3,9 @@ import Axios from 'axios';
 Axios.interceptors.request.use(
   (config) => {
     config.baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || '/api';
+    if (localStorage.getItem('token')) {
+      config.headers.token = localStorage.getItem('token');
+    }
     return config;
   },
   (error) => Promise.reject(error)
