97eca788a24056ed2bb30bd66622260fabfd2024
Jeanne <jeanne.grenet@gmail.com>
2023-12-08 03:20:41 +0100

    add pseudo

--- frontend/src/features/game/PageCreateGame.tsx
+++ frontend/src/features/game/PageCreateGame.tsx
@@ -7,26 +7,38 @@ import {
   Heading,
   Stack,
 } from '@chakra-ui/react';
+import { Formiz, useForm } from '@formiz/core';
+import { useTranslation } from 'react-i18next';
 
+import { FieldInput } from '@/components/FieldInput';
 import { Logo } from '@/components/Logo';
 import { Page, PageContent } from '@/components/Page';
 
 export default function PageDashboard() {
+  const form = useForm({ onSubmit: console.log });
+  const { t } = useTranslation(['layout']);
   return (
     <Page>
       <PageContent>
         <Card shadow="2xl" flex={1} rounded="2xl" p={6}>
           <CardHeader>
-            <Heading textAlign="center">Create Game</Heading>
+            <Heading textAlign="center">
+              {t('layout:createGame.createGame')}
+            </Heading>
           </CardHeader>
           <CardBody>
             <Stack flexDir="column" gap={8}>
               <Button color="white" colorScheme="teal">
-                New Game
+                {t('layout:createGame.newGame')}
               </Button>
-              <Button color="white" colorScheme="teal">
-                Load Game
-              </Button>
+              <Formiz connect={form} autoForm>
+                <FieldInput
+                  name="username"
+                  label={t('layout:createGame.username')}
+                  placeholder={t('layout:createGame.enterUsername')}
+                  required={t('layout:createGame.requiredUsername')}
+                ></FieldInput>
+              </Formiz>
               <Logo width="600" height="400"></Logo>
             </Stack>
           </CardBody>
